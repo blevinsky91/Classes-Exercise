b8fd0a5988a0e2d68bb74d95992f8c364a8dd8c3
Brandon Levinsky <blevinsky91@gmail.com>
2023-06-29 12:22:58 -0400

    ex1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+
+            Car car1 = new Car();
+            car1.Make = "Doge";
+            car1.Model = "Ram";
+            car1.Year = 2018;
+            Console.WriteLine(car1.Make);
+            Console.WriteLine(car1.Model);
+            Console.WriteLine(car1.Year);
         }
     }
 }
